13243570d997bbc560ec0198509737e40d1e2437
Tuah Hamid <tuah.edu@gmail.com>
2026-04-28 10:45:38 +0800

    alternative Nice3point template

---
+++ R25Buttons_Template/Application.cs
@@ -0,0 +1,26 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using R25Buttons_Template.Commands;
+
+namespace R25Buttons_Template;
+
+/// <summary>
+///     Application entry point
+/// </summary>
+[UsedImplicitly]
+public class Application : ExternalApplication
+{
+    public override void OnStartup()
+    {
+        CreateRibbon();
+    }
+
+
+    private void CreateRibbon()
+    {
+        var panel = Application.CreatePanel("Commands", "R25Buttons_Template");
+
+        panel.AddPushButton<StartupCommand>("Execute")
+            .SetImage("/R25Buttons_Template;component/Resources/Icons/RibbonIcon16.png")
+            .SetLargeImage("/R25Buttons_Template;component/Resources/Icons/RibbonIcon32.png");
+    }
+}
---
+++ R25Buttons_Template/Commands/StartupCommand.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
+using Autodesk.Revit.UI;
+
+namespace R25Buttons_Template.Commands;
+
+/// <summary>
+///     External command entry point.
+/// </summary>
+[UsedImplicitly]
+[Transaction(TransactionMode.Manual)]
+public class StartupCommand : ExternalCommand
+{
+    public override void Execute()
+    {
+        TaskDialog.Show(Application.ActiveUIDocument.Document.Title, "R25Buttons_Template");
+    }
+}
